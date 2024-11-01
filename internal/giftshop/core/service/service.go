a287486f27a5bf3b5e4aae0c99bcab3131a534b4
charmingruby <mrrubycanal3105@gmail.com>
2024-11-01 11:30:22 -0300

    feat: creates base giftshop repos and services

---
+++ internal/giftshop/core/service/service.go
@@ -0,0 +1,24 @@
+package service
+
+import "github.com/charmingruby/txgo/internal/giftshop/core/repository"
+
+type Service struct {
+	paymentRepo     repository.PaymentRepository
+	giftRepo        repository.GiftRepository
+	walletRepo      repository.WalletRepository
+	transactionRepo repository.TransactionRepository
+}
+
+func NewService(
+	paymentRepo repository.PaymentRepository,
+	giftRepo repository.GiftRepository,
+	walletRepo repository.WalletRepository,
+	transactionRepo repository.TransactionRepository,
+) *Service {
+	return &Service{
+		paymentRepo:     paymentRepo,
+		giftRepo:        giftRepo,
+		walletRepo:      walletRepo,
+		transactionRepo: transactionRepo,
+	}
+}
